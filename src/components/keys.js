e11a1027906aa43b55dae7d4e2c98b4f783ce19a
Apoorv Chauhan <01apoorv@gmail.com>
2023-02-19 17:17:05 -0500

    i am absolute dogwater at coding

--- src/components/keys.js
+++ src/components/keys.js
@@ -1,5 +1,5 @@
 const keys = {
     client_id: 'b8140e9fd3af43cdb2ea05ba116936e0', // Your client id
-    redirect_uri: 'http://spotify-app.pages.dev/results' // Your redirect uri
+    redirect_uri: 'https://spotify-app.pages.dev/results' // Your redirect uri
 }
 export default keys
